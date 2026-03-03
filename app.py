5f10a30456ec5648511f55f2091115fdb5eae18a
olkragsterman-cloud <olkragsterman@asbarcelona.com>
2026-03-03 08:18:08 +0000

    -

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask , render_template, request
 
 app = Flask(__name__)
 
-# TODO: Add your routes below this line
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+    name = request.args.get("name", "world")
+    return render_template("greet.html", name=name)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
